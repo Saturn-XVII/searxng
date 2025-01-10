07bef1478322d07a2bc3570414192c756df3fdbd
Urbigmumz <140040348+Urbigmumz-dev@users.noreply.github.com>
2025-01-10 08:37:12 +0000

    uhhhh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt && pip install -r requirements-dev.txt
-sudo service redis-server start
-sudo service redis-server status
+#sudo service redis-server start
+#sudo service redis-server status
 redis-cli ping
 python3 -m venv venv
-python3 ./searx/webapp.py
+python3 searx/webapp.py
